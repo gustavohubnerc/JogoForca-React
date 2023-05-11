9c05752e8ff0443941be5916dfdaa62a914cf0a8
gustavohubner <hubnergc@gmail.com>
2023-05-11 12:38:05 -0300

    layout e componentização

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Jogo from './components/Jogo'
+import Letras from './components/Letras'
+import './palavras';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const [isEnabled, setIsEnabled] = useState(false);  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Jogo/>
+      <Letras/>
+    </>  
   );
 }
+
 
 export default App;
---
+++ src/components/Letras.js
@@ -0,0 +1,12 @@
+export default function Letras() {
+    
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
+    return (
+        <div className="container-inferior">
+            <div className="teclado">
+                {alfabeto.map(letra => <button className="letra">{letra}</button>)}
+            </div>  
+        </div>          
+    );  
+}
