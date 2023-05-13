7a91e98516b9304c6f1fe671cbe130622a3c4f6f
gustavohubner <hubnergc@gmail.com>
2023-05-13 17:06:56 -0300

    salvando teste

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-  import React, { useState } from 'react';
-  import Jogo from './components/Jogo';
-  import Letras from './components/Letras';
-  import Palavra from './components/PalavraSecreta';
-  import palavras from './palavras';
+import React, { useState } from 'react';
+import Jogo from './components/Jogo';
+import Letras from './components/Letras';
+import Palavra from './components/PalavraSecreta';
+import palavras from './palavras';
 
-  export default function App() {
-    const [isEnabled, setIsEnabled] = useState(false);
-    const [palavraEscolhida, setPalavraEscolhida] = useState('');
-    const [letrasPalavra, setLetrasPalavra] = useState([]);
-    const [letrasDescobertas, setLetrasDescobertas] = useState([]);
+export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [palavraEscolhida, setPalavraEscolhida] = useState('');
+  const [letrasPalavra, setLetrasPalavra] = useState([]);
+  const [letrasDescobertas, setLetrasDescobertas] = useState([]);
 
-    function escolherPalavra() {
-      const indice = Math.floor(Math.random() * palavras.length);
-      const palavra = palavras[indice];
-      setPalavraEscolhida(palavra);
-      setLetrasPalavra(palavra.split(''));
-      setLetrasDescobertas([]);
-    }
+  function escolherPalavra() {
+    const indice = Math.floor(Math.random() * palavras.length);
+    const palavra = palavras[indice];
+    setPalavraEscolhida(palavra);
+    setLetrasPalavra(palavra.split(''));
+    setLetrasDescobertas([]);
+  }
 
-    function handleLetraEscolhida(letra) {
-      setLetrasDescobertas((letrasDescobertas) =>
-        letrasDescobertas.concat(letra)
-      );
-    }
-
-    return (
-      <>
-        <Jogo onEscolherPalavra={escolherPalavra} setIsEnabled={setIsEnabled} />
-        <Palavra palavra={palavraEscolhida} letrasDescobertas={letrasDescobertas} />
-        <Letras
-          isEnabled={isEnabled}
-          letrasEscolhidas={letrasDescobertas}
-          palavraEscolhida={letrasPalavra}
-          onLetraEscolhida={handleLetraEscolhida}
-        />
-
-        
-      </>
+  function handleLetraEscolhida(letra) {
+    setLetrasDescobertas((letrasDescobertas) =>
+      letrasDescobertas.concat(letra)
     );
   }
+
+  return (
+    <>
+      <Jogo onEscolherPalavra={escolherPalavra} setIsEnabled={setIsEnabled} />
+      <Palavra palavra={palavraEscolhida} letrasDescobertas={letrasDescobertas} />
+      <Letras
+        isEnabled={isEnabled}
+        letrasEscolhidas={letrasDescobertas}
+        palavraEscolhida={letrasPalavra}
+        onLetraEscolhida={handleLetraEscolhida}
+      />
+
+      
+    </>
+  );
+}
